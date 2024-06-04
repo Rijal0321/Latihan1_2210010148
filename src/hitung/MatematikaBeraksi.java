04d4bd8bb87e68ca31a0a1ef2609ac4bb82f67c8
Rijal0321 <rijal.kjr@gmail.com>
2024-06-04 23:13:39 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika rijal = new Matematika(4,8);
+        
+        System.out.println("Hasil Penjumlahan : "+rijal.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+rijal.setPengurangan());
+        System.out.println("Hasil Perkalian : "+rijal.setPerkalian());
+        System.out.println("Hasil Pembagian : "+rijal.setPembagian());
+    }
 }
